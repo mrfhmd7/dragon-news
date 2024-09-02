20905daa23c56b0bbbcdadceffdc00c3e1a90c68
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-03 00:18:00 +0600

    marquee and navbar from bootstarp

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import Footer from '../pages/Shared/Footer/Footer';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import LeftNav from '../pages/Shared/LeftNav/LeftNav';
 import RightNav from '../pages/Shared/RightNav/RightNav';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Main = () => {
      return (
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import logo from '../../../assets/logo.png';
 import moment from 'moment';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import Marquee from 'react-fast-marquee';
 
 const Header = () => {
      return (
-          <div>
+          <Container>
                <div className="text-center">
                     <img src={logo} alt="" />
                     <p><small className='text-secondary'>Journalism Without Fear or Favour</small></p>
                     <p>{moment().format('dddd, MMMM D, YYYY')}</p>
                </div>
-          </div>
+               <div className='d-flex' style={{ backgroundColor: '#F3F3F3' }}>
+                    <Button variant="danger">Breaking...</Button>
+                    <Marquee>
+                         I can be a React component, multiple React components, or just some text..... I can be a React component, multiple React components, or just some text.
+                    </Marquee>
+               </div>
+               <Navbar collapseOnSelect expand="lg" className="bg-light">
+                    <Container>
+                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                         <Navbar.Collapse id="responsive-navbar-nav">
+                              <Nav className="mx-auto">
+                                   <Nav.Link href="#features">Home</Nav.Link>
+                                   <Nav.Link href="#pricing">About</Nav.Link>
+                                   <Nav.Link href="#pricing">Career</Nav.Link>
+                              </Nav>
+                              <Nav>
+                                   <Nav.Link href="#deets">Profile</Nav.Link>
+                                   <Nav.Link eventKey={2} href="#memes">
+                                   <Button variant="secondary">Login</Button>
+                                   </Nav.Link>
+                              </Nav>
+                         </Navbar.Collapse>
+                    </Container>
+               </Navbar>
+          </Container>
      );
 };
 
