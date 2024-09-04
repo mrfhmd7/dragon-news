cc8231c882321a1b362ae24658342c7d502e0f3f
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-04 09:02:53 +0600

    showing category in left nav

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -6,7 +6,7 @@ import Marquee from 'react-fast-marquee';
 
 const Header = () => {
      return (
-          <Container>
+          <Container className='mt-4'>
                <div className="text-center">
                     <img src={logo} alt="" />
                     <p><small className='text-secondary'>Journalism Without Fear or Favour</small></p>
--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const LeftNav = () => {
+
+     const [categories, setCategories] = useState([]);
+
+     useEffect(() => {
+          fetch('http://localhost:5000/categories')
+               .then(res => res.json())
+               .then(data => setCategories(data))
+          // .then(error => console.error(error));
+     }, [])
+
      return (
           <div>
-               <h2>Left Nav bar</h2>
+               <h4>All Category</h4>
+               <div className='ps-4'>
+                    {
+                         categories.map(category => <p
+                              key={category.id}
+                         >
+                              <NavLink
+                                   to={`/category/${category.id}`}
+                                   className='text-decoration-none text-black'
+                                   activeClassName="active-category"
+                              >
+                                   {category.name}
+                              </NavLink>
+                         </p>)
+                    }
+               </div>
           </div>
      );
 };
