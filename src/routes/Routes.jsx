87b79f1b9984451739755f369fa08a17ba7390e8
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-15 15:49:21 +0600

    setup new layout for news

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
+import NewsLayout from "../layouts/NewsLayout";
+import News from "../pages/News/News/News";
 
 const router = createBrowserRouter([
      {
@@ -15,6 +17,16 @@ const router = createBrowserRouter([
                {
                     path: '/category/:id',
                     element: <Category/>
+               }
+          ]
+     },
+     {
+          path: 'news',
+          element: <NewsLayout />,
+          children: [
+               {
+                    path: ':id',
+                    element: <News/>
                }
           ]
      }
