8554d5971501ae0c5354f832f3132d8ab1c66e4e
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-16 04:46:07 +0600

    Log in from create

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
      return (
-          <div>
-               <h2>This is Login</h2>
-          </div>
+          <Container className="d-flex justify-content-center align-items-center mt-5">
+               <Row className="w-100">
+                    <Col md={6} lg={5} className="mx-auto">
+                         <div className="shadow p-4 rounded bg-white">
+                              <h3 className="text-center mb-4">Login your account</h3>
+                              <Form>
+                                   <Form.Group controlId="formBasicEmail" className="mb-3">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" name="email" placeholder="Enter your email" required />
+                                   </Form.Group>
+
+                                   <Form.Group controlId="formBasicPassword" className="mb-4">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" name="password" placeholder="Enter your password" required/>
+                                   </Form.Group>
+
+                                   <Button variant="dark" type="submit" className="w-100">
+                                        Login
+                                   </Button>
+                                   <Form.Text className='text-success'>
+
+                                   </Form.Text>
+                                   <Form.Text className='text-danger'>
+
+                                   </Form.Text>
+                              </Form>
+                              <div className="text-center mt-3">
+                                   <span>Do not Have An Account? </span>
+                                   <Link to="/register" className="text-danger">Register</Link>
+                              </div>
+                         </div>
+                    </Col>
+               </Row>
+          </Container>
      );
 };
 
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Register = () => {
      return (
           <div>
-               <h2>This is Register</h2>
+               
           </div>
      );
 };
