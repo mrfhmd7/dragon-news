3291989562a384af9e6f37980bebf8e217c16420
mrfhmd7 <mrf.hmd7@gmail.com>
2024-09-17 01:16:15 +0600

    get loggedUser

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Login = () => {
+     const { signIn } = useContext(AuthContext);
+
+     const handleLogin = event => {
+          event.preventDefault();
+
+          const form = event.target;
+          const email = form.email.value;
+          const password = form.password.value;
+          console.log(email, password);
+
+          signIn(email, password)
+               .then(result => {
+                    const loggedUser = result.user;
+                    console.log(loggedUser);
+               })
+               .catch(error => {
+                    console.log(error)
+               });
+     }
+
      return (
           <Container className="d-flex justify-content-center align-items-center mt-5">
                <Row className="w-100">
                     <Col md={6} lg={5} className="mx-auto">
                          <div className="shadow p-4 rounded bg-white">
                               <h3 className="text-center mb-4">Login your account</h3>
-                              <Form>
+                              <Form onSubmit={handleLogin}>
                                    <Form.Group controlId="formBasicEmail" className="mb-3">
                                         <Form.Label>Email</Form.Label>
                                         <Form.Control type="email" name="email" placeholder="Enter your email" required />
@@ -17,7 +38,7 @@ const Login = () => {
 
                                    <Form.Group controlId="formBasicPassword" className="mb-4">
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" name="password" placeholder="Enter your password" required/>
+                                        <Form.Control type="password" name="password" placeholder="Enter your password" required />
                                    </Form.Group>
 
                                    <Button variant="dark" type="submit" className="w-100">
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -12,11 +12,15 @@ const AuthProvider = ({ children }) => {
           return createUserWithEmailAndPassword(auth, email, password);
      }
 
+     const signIn = (email, password) => { 
+          return signInWithEmailAndPassword(auth, email, password);
+     }
+
      const authInfo = {
           user,
           createUser,
+          signIn,
           
-
      };
      return (
           <AuthContext.Provider value={authInfo}>
